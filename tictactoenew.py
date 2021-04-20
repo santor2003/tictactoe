81aa4f611abef993558368d533bc5b1e75730831
santor2003 <santor2003@gmail.com>
2021-04-19 23:10:28 -0500

    tictactoenew

---
+++ tictactoenew.py
@@ -0,0 +1,10 @@
+board= [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+]
+
+def print_board(board):
+    for row in board:
+        for slot in row:
+            print(slot, end="")
