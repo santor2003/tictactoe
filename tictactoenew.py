4e38ee3c7f57e8c380c0e354b9a671dfaaa007f3
santor2003 <santor2003@gmail.com>
2021-04-19 23:56:11 -0500

    added board

--- tictactoenew.py
+++ tictactoenew.py
@@ -7,4 +7,10 @@ board= [
 def print_board(board):
     for row in board:
         for slot in row:
-            print(slot, end="")
+            print(f"{slot}", end="")
+        print()
+
+print_board(board)
+
+while True:
+    user_input= input(" Enter position from 1 through 9")
